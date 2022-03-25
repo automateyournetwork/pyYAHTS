458e0ab5df94f8b0bbd176c72f9bec8e540858fd
John Capobianco <ptcapo@gmail.com>
2022-03-25 18:49:58 -0400

    working with click

--- src/pyYAHTS/pyYAHTS.py
+++ src/pyYAHTS/pyYAHTS.py
@@ -1,10 +1,12 @@
 import sys
-import logging
+import click
 import json
+import logging
 from pyats.topology import Testbed, Device
 from genie import testbed
+from rich import print_json
 
-class get_json():
+class GetJson():
     def __init__(self, hostname, os, username, password, command):
         self.hostname = hostname
         self.os = os
@@ -12,6 +14,9 @@ class get_json():
         self.password = password
         self.command = command
         self.print_json()
+
+    def print_json(self):
+        print_json(json.dumps(self.capture_state(), indent=4, sort_keys=True))
 
     # Create Testbed
     def connect_device(self):
@@ -72,9 +77,15 @@ class get_json():
                     command_output = f"Cannot Learn { self.command }"
             return(command_output)
 
-    def print_json(self):
-        print(json.dumps(self.capture_state(), indent=4, sort_keys=True))
-        return
+@click.command()
+@click.option('--hostname', prompt='Hostname', help='Hostname of device - must match the device', required=True)
+@click.option('--os', prompt='OS', type=click.Choice(['iosxe', 'nxos', 'iosxr'], case_sensitive=True), help='OS of device - must match the device', required=True)
+@click.option('--username', prompt='Username', help='Username', required=True)
+@click.option('--password', prompt='Password', help='Password of the user', required=True)
+@click.option('--command', prompt='Command', help='A valid pyATS Learn Function (i.e. ospf) or valid CLI Show Command (i.e. "show ip interface brief")', required=True)
+def cli(hostname, os, username, password, command):
+    invoke_class = GetJson(hostname, os, username, password, command)
+    invoke_class.print_json()
 
 if __name__ == "__main__":
-    get_json(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])        
+    cli()    
