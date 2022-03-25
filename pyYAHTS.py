668789c91117720d1a86b3cede6aaca4c2fbf96a
John Capobianco <ptcapo@gmail.com>
2022-03-25 19:24:46 -0400

    small fix

--- pyYAHTS.py
+++ pyYAHTS.py
@@ -80,7 +80,7 @@ class GetJson():
 @click.option('--hostname', prompt='Hostname', help='Hostname of device - must match the device', required=True)
 @click.option('--os', prompt='OS', type=click.Choice(['ios', 'iosxe', 'nxos', 'iosxr'], case_sensitive=True), help='OS of device - must match the device', required=True)
 @click.option('--username', prompt='Username', help='Username', required=True)
-@click.option('--password', prompt=True, hide_input=Truehelp="User Password", required=True)
+@click.option('--password', prompt=True, hide_input=True, help="User Password", required=True)
 @click.option('--command', prompt='Command', help='A valid pyATS Learn Function (i.e. ospf) or valid CLI Show Command (i.e. "show ip interface brief")', required=True)
 def cli(hostname, os, username, password, command):
     invoke_class = GetJson(hostname, os, username, password, command)
